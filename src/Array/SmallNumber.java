b6f9d702a4273845759d3dce80359b9adcae8f6e
iyuna <dbsk2778@unidcompany.com>
2024-04-04 23:34:49 +0900

    #feature/Arrays add: 백준 10871 X 보다 작은 수

---
+++ src/Array/SmallNumber.java
@@ -0,0 +1,30 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SmallNumber {
+
+  //정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+
+  public static void main(String[] args) {
+    List<Integer> smallArr = new ArrayList<>();
+    Scanner scan = new Scanner(System.in);
+
+    int N = scan.nextInt();
+    int X = scan.nextInt();
+
+    for (int i = 0; i < N; i++) {
+      int num = scan.nextInt();
+      if (num < X) {
+        smallArr.add(num);
+      }
+    }
+    scan.close();
+
+    for (int num : smallArr) {
+      System.out.print(num + " ");
+    }
+  }
+}
