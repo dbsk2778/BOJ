f2e8ee27c191d923286aea1ea10d6c5dd651f235
dbsk2778 <dbsk2778@gmail.com>
2024-08-02 13:13:36 +0900

    백준 단계별 문제 5단계 문자열: 나머지 문제 추가

---
+++ src/BOJStep/String/BOJ5622.java
@@ -0,0 +1,40 @@
+package BOJStep.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ5622 {
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String code = br.readLine();
+        int count = 0;
+
+        for (char c : code.toCharArray()) {
+            if (c >= 'A' && c <= 'C') {
+                count += 3;
+            } else if (c >= 'D' && c <= 'F') {
+                count += 4;
+            } else if (c >= 'G' && c <= 'I') {
+                count += 5;
+            } else if (c >= 'J' && c <= 'L') {
+                count += 6;
+            } else if (c >= 'M' && c <= 'O') {
+                count += 7;
+            } else if (c >= 'P' && c <= 'S') {
+                count += 8;
+            } else if (c >= 'T' && c <= 'V') {
+                count += 9;
+            } else if (c >= 'W' && c <= 'Z') {
+                count += 10;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
+
--- src/BOJStep/String/Basic.java
+++ src/BOJStep/String/Basic.java
@@ -1,7 +1,10 @@
 package BOJStep.String;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Basic {
 
@@ -9,6 +12,8 @@ public class Basic {
         BOJ27866();
         BOJ2743();
         BOJ9086();
+        BOJ1152();
+        BOJ1152_2();
     }
 
     public static void BOJ27866() {
@@ -37,5 +42,21 @@ public class Basic {
             bw.write(str.charAt(0) + "" + str.charAt(str.length()-1) + "\n");
         }
         bw.flush();
+    }
+
+    public static void BOJ1152() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strs = br.readLine().split(" ");
+
+        // isBlank()와 toList()는 자바 17버전에서만 사용 가능, 11버전 불가
+        List<String> l = Arrays.stream(strs).filter(str -> !str.isBlank()).toList();
+
+        System.out.println(l.size());
+    }
+
+    public static void BOJ1152_2() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        System.out.println(st.countTokens());
     }
 }
