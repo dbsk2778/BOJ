a1ba949a71fecbd9af8f4ea3119dde4c4ba11012
dbsk2778 <dbsk2778@gmail.com>
2024-08-04 23:21:32 +0900

    백준 단계별 문제 8단계 일반 수학 1 : 2903 분수 찾기 추가

---
+++ src/BOJStep/Math/BOJ1193.java
@@ -0,0 +1,62 @@
+package BOJStep.Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1193 {
+    public static void main(String[] args) throws IOException {
+        solution();
+        solution2();
+    }
+
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int num = 0, sum = 0, count = 0;
+
+        for (int i = 0; i < X; i += num) {
+            num++;
+            sum += num;
+            count++;
+        }
+
+        X = sum - X;
+
+        int x, y;
+        if (count % 2 == 0) {
+            x = count;
+            y = 1;
+            for (int i = 0; i < X; i++) {
+                y++;
+                x--;
+            }
+
+        } else {
+            x = 1;
+            y = count;
+            for (int i = 0; i < X; i++) {
+                y--;
+                x++;
+            }
+
+        }
+        System.out.println(x + "/" + y);
+    }
+
+    public static void solution2() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int num = 1;
+
+        while ( X > num ) {
+            X -= num++;
+        }
+
+        if (num % 2 == 0) {
+            System.out.println(X + "/" + (num + 1 - X));
+        } else {
+            System.out.println((num + 1 - X) + "/"  + X);
+        }
+    }
+}
