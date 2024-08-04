9130d3bc1a4cb11d3a1201cf37cab7ca1ad1f893
dbsk2778 <dbsk2778@gmail.com>
2024-08-04 18:02:52 +0900

    백준 단계별 문제 8단계 일반 수학 1 : 2745 진법 변환 추가

---
+++ src/BOJStep/Math/BOJ2745.java
@@ -0,0 +1,22 @@
+package BOJStep.Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2745 {
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String N = st.nextToken();
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(Integer.parseInt(N, B));
+    }
+}
