4ff1584ffc8edcdd60f19126b8ec5c02c66d648c
dbsk2778 <dbsk2778@gmail.com>
2024-08-05 15:02:15 +0900

    백준 단계별 문제 Level9 1978 소수 찾기 문제 추가

---
+++ src/BOJStep/Level9/BOJ1978.java
@@ -0,0 +1,34 @@
+package BOJStep.Level9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1978 {
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (num != 1 && isPrime(num)) count++;
+        }
+
+        System.out.println(count);
+    }
+
+    private static boolean isPrime(int num) {
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
